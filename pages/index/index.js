3e1f5adec3e51d0d866d68023dbf4820fb2d0c64
enyobao <793169740@qq.com>
2017-02-05 22:08:18 +0800

    lunbotu

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,54 @@ Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
+    details:[
+      {
+        title:"色彩云南 色彩云南 色彩云南 色彩云南",
+        price:"¥2990",
+        date:"1/30",
+        departure:"昆明出发",
+        days:"8日行程",
+        imgsrc:"../img/car.jpeg"
+      },
+      {
+        title:"色彩云南 色彩云南 色彩云南 色彩云南",
+        price:"¥2990",
+        date:"2/30",
+        departure:"昆明出发",
+        days:"8日行程",
+        imgsrc:"../img/car.jpeg"
+      },
+      {
+        title:"色彩云南 色彩云南 色彩云南 色彩云南",
+        price:"¥2990",
+        date:"3/30",
+        departure:"昆明出发",
+        days:"8日行程",
+        imgsrc:"../img/car.jpeg"
+      }
+    ],
+    imgUrls:[
+      {
+        link:"",
+        url:"../img/dengshan.jpeg"
+      },
+      {
+        link:"",
+        url:"../img/run2.jpeg"
+      },
+      {
+        link:"",
+        url:"../img/bike.jpeg"
+      },
+      {
+        link:"",
+        url:"../img/hike.jpeg"
+      }
+    ],
+    indicatorDots:true,
+    autoplay:true,
+    interval:5000,
+    duration:1000,
     currentTab: 0
   },
   //事件处理函数
